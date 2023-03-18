048d0d15756e8374125081bd7f77171bcad87572
rakovkas <sadzidarakovac@gmail.com>
2023-03-18 21:05:21 +0100

    insert updated

--- php/inc/insert.php
+++ php/inc/insert.php
@@ -1,6 +1,6 @@
-<?php
-//unos podataka preko forme u bazu
 
+//unos podataka preko forme u bazu
+/*
 if (isset($_POST['submit'])) {
     $Ime = $_POST ['Ime'];
     $email = $_POST['email'];
@@ -22,4 +22,53 @@ if (isset($_POST['submit'])) {
     else {
         echo "Podaci nisu unijeti. Sva polja moraju biti popunjena!";
     }
-}
+} */
+
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <title>Insert Page page</title>
+</head>
+ 
+<body>
+        <?php
+ 
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost", "root", "", "db_pridruzi");
+         
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
+         
+        // Taking all 5 values from the form data(input)
+        $Ime =  $_REQUEST['Ime'];
+        $email = $_REQUEST['email'];
+         
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO tbl_pridruzi  VALUES ('$Ime','$email')";
+         
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully."
+                . " Please browse your localhost php my admin"
+                . " to view the updated data</h3>";
+ 
+            echo nl2br("\n$Ime\n $email");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. "
+                . mysqli_error($conn);
+        }
+         
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    
+</body>
+ 
+</html>
