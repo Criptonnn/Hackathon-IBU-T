3b57b65d33c841065f64b396d6670bec68a2600f
rakovkas <sadzidarakovac@gmail.com>
2023-03-18 19:37:44 +0100

    pridruzi, baza podataka

---
+++ php/inc/insert.php
@@ -0,0 +1,27 @@
+<?php
+//unos podataka preko forme u bazu
+
+if (isset($_POST['submit'])) {
+    $Ime = $_POST ['Ime'];
+    $email = $_POST['email'];
+
+    require 'connect.php';
+
+    $Ime = mysqli_real_escape_string($conn, $Ime);
+    $email = mysqli_real_escape_string($conn, $email);
+
+    if (!empty($Ime) && !empty($email)) {
+
+        $upit = "INSERT INTO tbl_pridruzi (Ime, e-mail) 
+        VALUES ('{$Ime}', '{$email}')";
+
+        mysqli_query($conn, $upit);
+
+        echo "Podaci su unijeti!";
+    } 
+    else {
+        echo "Podaci nisu unijeti. Sva polja moraju biti popunjena!";
+    }
+
+    
+}
